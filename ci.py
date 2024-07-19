bbccf456bbafc3a33eabe092e0ec2662ce935280
MANI <56706691+pavodi-NM@users.noreply.github.com>
2024-07-19 14:27:45 +0900

    Update ci.py

--- ci.py
+++ ci.py
@@ -2,4 +2,4 @@ import wandb
 
 print(f"the version of wandb is : {wandb.__version__}")
 
-assert wandb__version__ == '2.1.01', f'expected 2.1.01, but got {wandb__version__}'
+assert wandb__version__ == '2.1.01', f'expected 2.1.01, but got a different version: {wandb__version__}'
